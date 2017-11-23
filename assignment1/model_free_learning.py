b607a11785840e6a5ef4183a984cd707c555c03d
zlpure <zengliangcs@gmail.com>
2017-11-23 15:07:07 +0800

    model_free_learning: q-learning(off-policy), SARSA(on-policy)

--- assignment1/model_free_learning.py
+++ assignment1/model_free_learning.py
@@ -6,101 +6,154 @@ import numpy as np
 import gym
 import time
 from lake_envs import *
+import matplotlib.pyplot as plt
 
 def learn_Q_QLearning(env, num_episodes=2000, gamma=0.95, lr=0.1, e=0.8, decay_rate=0.99):
-  """Learn state-action values using the Q-learning algorithm with epsilon-greedy exploration strategy.
-  Update Q at the end of every episode.
-
-  Parameters
-  ----------
-  env: gym.core.Environment
-    Environment to compute Q function for. Must have nS, nA, and P as
-    attributes.
-  num_episodes: int 
-    Number of episodes of training.
-  gamma: float
-    Discount factor. Number in range [0, 1)
-  learning_rate: float
-    Learning rate. Number in range [0, 1)
-  e: float
-    Epsilon value used in the epsilon-greedy method. 
-  decay_rate: float
-    Rate at which epsilon falls. Number in range [0, 1)
-
-  Returns
-  -------
-  np.array
-    An array of shape [env.nS x env.nA] representing state, action values
-  """
-  
-  ############################
-  # YOUR IMPLEMENTATION HERE #
-  ############################
-
-  return np.zeros((env.nS, env.nA))
-
-def learn_Q_SARSA(env, num_episodes=2000, gamma=0.95, lr=0.1, e=0.8, decay_rate=0.99):
-  """Learn state-action values using the SARSA algorithm with epsilon-greedy exploration strategy
-  Update Q at the end of every episode.
-
-  Parameters
-  ----------
-  env: gym.core.Environment
-    Environment to compute Q function for. Must have nS, nA, and P as
-    attributes.
-  num_episodes: int 
-    Number of episodes of training.
-  gamma: float
-    Discount factor. Number in range [0, 1)
-  learning_rate: float
-    Learning rate. Number in range [0, 1)
-  e: float
-    Epsilon value used in the epsilon-greedy method. 
-  decay_rate: float
-    Rate at which epsilon falls. Number in range [0, 1)
-
-  Returns
-  -------
-  np.array
-    An array of shape [env.nS x env.nA] representing state-action values
-  """
-
-  ############################
-  # YOUR IMPLEMENTATION HERE #
-  ############################
-
-  return np.ones((env.nS, env.nA))
-
-def render_single_Q(env, Q):
-  """Renders Q function once on environment. Watch your agent play!
+    """Learn state-action values using the Q-learning algorithm with epsilon-greedy exploration strategy.
+    Update Q at the end of every episode.
 
     Parameters
     ----------
     env: gym.core.Environment
-      Environment to play Q function on. Must have nS, nA, and P as
-      attributes.
+        Environment to compute Q function for. Must have nS, nA, and P as
+        attributes.
+    num_episodes: int 
+        Number of episodes of training.
+    gamma: float
+        Discount factor. Number in range [0, 1)
+    learning_rate: float
+        Learning rate. Number in range [0, 1)
+    e: float
+        Epsilon value used in the epsilon-greedy method. 
+    decay_rate: float
+        Rate at which epsilon falls. Number in range [0, 1)
+
+    Returns
+    -------
+    np.array
+        An array of shape [env.nS x env.nA] representing state, action values
+    """
+    ############################
+    # YOUR IMPLEMENTATION HERE #
+    q_value = np.zeros([env.nS, env.nA])  
+    for i in range(num_episodes):
+        done = False
+        state = env.reset()
+        while not done:
+            if np.random.rand() > e:
+                action = np.argmax(q_value[state])
+            else:
+                action = np.random.randint(env.nA)
+            nextstate, reward, done, _ = env.step(action)
+            q_value[state][action] = (1-lr)*q_value[state][action]+lr*(reward+gamma*np.max(q_value[nextstate]))
+            state = nextstate
+        if i%10 == 0:
+            e *= decay_rate        
+    '''
+    print np.mean(q_value)
+    
+    plt.plot(np.arange(num_episodes),np.array(score))
+    plt.title('The running average score of the Q-learning agent')
+    plt.xlabel('traning episodes')
+    plt.ylabel('score')
+    #plt.show()
+    plt.savefig('c.jpg')
+    '''
+    ############################
+    return q_value
+
+def learn_Q_SARSA(env, num_episodes=2000, gamma=0.95, lr=0.1, e=0.8, decay_rate=0.99):
+    """Learn state-action values using the SARSA algorithm with epsilon-greedy exploration strategy
+    Update Q at the end of every episode.
+
+    Parameters
+    ----------
+    env: gym.core.Environment
+        Environment to compute Q function for. Must have nS, nA, and P as
+        attributes.
+    num_episodes: int 
+        Number of episodes of training.
+    gamma: float
+        Discount factor. Number in range [0, 1)
+    learning_rate: float
+        Learning rate. Number in range [0, 1)
+    e: float
+        Epsilon value used in the epsilon-greedy method. 
+    decay_rate: float
+        Rate at which epsilon falls. Number in range [0, 1)
+
+    Returns
+    -------
+    np.array
+        An array of shape [env.nS x env.nA] representing state-action values
+    """
+    ############################
+    # YOUR IMPLEMENTATION HERE #
+    q_value = np.zeros([env.nS, env.nA])  
+    for i in range(num_episodes):
+        done = False
+        state = env.reset()
+        if np.random.rand() > e:
+            action = np.argmax(q_value[state])
+        else:
+            action = np.random.randint(env.nA)
+        while not done:
+            nextstate, reward, done, _ = env.step(action)
+            if np.random.rand() > e:
+                nextaction = np.argmax(q_value[nextstate])
+            else:
+                nextaction = np.random.randint(env.nA)
+            q_value[state][action] = (1-lr)*q_value[state][action]+lr*(reward+gamma*q_value[nextstate][nextaction])
+            state = nextstate
+            action = nextaction
+        if i%10 == 0:
+            e *= decay_rate
+    ############################
+
+    return q_value
+
+def render_single_Q(env, Q):
+    """Renders Q function once on environment. Watch your agent play!
+
+    Parameters
+    ----------
+    env: gym.core.Environment
+        Environment to play Q function on. Must have nS, nA, and P as
+        attributes.
     Q: np.array of shape [env.nS x env.nA]
-      state-action values.
-  """
+        state-action values.
+    """
 
-  episode_reward = 0
-  state = env.reset()
-  done = False
-  while not done:
-    env.render()
-    time.sleep(0.5) # Seconds between frames. Modify as you wish.
-    action = np.argmax(Q[state])
-    state, reward, done, _ = env.step(action)
-    episode_reward += reward
+    episode_reward = 0
+    state = env.reset()
+    done = False
+    while not done:
+        #env.render()  #show frames 
+        #time.sleep(0.5) # Seconds between frames. Modify as you wish.
+        action = np.argmax(Q[state])
+        state, reward, done, _ = env.step(action)
+        episode_reward += reward
 
-  print "Episode reward: %f" % episode_reward
-
+    #print "Episode reward: %f" % episode_reward
+    return episode_reward
+    
 # Feel free to run your own debug code in main!
 def main():
-  env = gym.make('Stochastic-4x4-FrozenLake-v0')
-  Q = learn_Q_QLearning(env)
-  # Q = learn_Q_SARSA(env)
-  render_single_Q(env, Q)
-
+    env = gym.make('Stochastic-4x4-FrozenLake-v0')
+    Q = learn_Q_QLearning(env)
+    #Q = learn_Q_SARSA(env)
+    score = []
+    for i in range(1000):
+        episode_reward = render_single_Q(env, Q)
+        score.append(episode_reward)
+    for i in range(len(score)):
+        score[i] = np.mean(score[:i+1])
+    plt.plot(np.arange(1000),np.array(score))
+    plt.title('The running average score of the Q-learning agent')
+    plt.xlabel('traning episodes')
+    plt.ylabel('score')
+    #plt.show()
+    plt.savefig('c.jpg')
+           
 if __name__ == '__main__':
     main()
